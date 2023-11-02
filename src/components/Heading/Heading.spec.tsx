c5161ce59256d2a0d15e022a06b71a12f95f43ee
luczod <kzod5611@gmail.com>
2023-11-02 00:26:47 -0300

    no-unused-vars ignore

--- src/components/Heading/Heading.spec.tsx
+++ src/components/Heading/Heading.spec.tsx
@@ -1,8 +1,18 @@
-import { Heading } from ".";
 import { renderTheme } from "../../styles/render-theme";
+import { Heading } from ".";
+import { screen } from "@testing-library/react";
+import { themefn } from "../../styles/themefn";
 
 describe("<Heading/>", () => {
+  beforeEach(() => {
+    jest.spyOn(console, "warn").mockImplementation(() => {});
+  });
   it("should render with default values", () => {
-    renderTheme(<Heading>Texto do teste</Heading>);
+    const { debug } = renderTheme(<Heading variant="h2">Texto</Heading>);
+    const heading = screen.getByRole("heading", { name: "Texto" });
+    expect(heading).toHaveStyle({
+      color: themefn.colors.primaryColor,
+      "font-size": themefn.font.sizes.xl,
+    });
   });
 });
--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,3 @@
 import "@testing-library/jest-dom";
+jest.spyOn(console, "error").mockImplementation(() => {});
+jest.spyOn(console, "warn").mockImplementation(() => {});
