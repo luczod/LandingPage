ae91675757032dee80439372abcd996fb3a663f0
luczod <kzod5611@gmail.com>
2023-11-01 23:43:41 -0300

    no coverage

--- src/components/Heading/Heading.spec.tsx
+++
@@ -1,8 +0,0 @@
-import { Heading } from ".";
-import { renderTheme } from "../../styles/render-theme";
-
-describe("<Heading/>", () => {
-  it("should render with default values", () => {
-    renderTheme(<Heading>Texto do teste</Heading>);
-  });
-});
--- src/templates/App/Home.spec.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import { Home } from ".";
-
-it("renders learn react link", () => {
-  render(<Home />);
-  const linkElement = screen.getByText(/Hello/i);
-  expect(linkElement).toBeInTheDocument();
-});
