a361a8747dbc950a3c0ccb3d9323a5040b8e6ba2
luczod <kzod5611@gmail.com>
2023-11-04 21:05:49 -0300

    new config test vite

--- jest.config.ts
+++ jest.config.ts
@@ -137,7 +137,7 @@ const config: Config = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: ["<rootDir>/src/test/setup.ts"],
+  setupFilesAfterEnv: ["<rootDir>/__test__/setup.ts"],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
